8d204d97bec50d996f7e5212bc9f704a5ae5a21e
James <james@jameslean.net>
2015-09-08 19:14:18 +0100

    Complete memtest

--- memtest.asm
+++ memtest.asm
@@ -1,81 +1,88 @@
      * 
      * Tests the full 8k of memory apart from the memory occupied by this program
+     * Writes all 1's on first pass, then all 0's on second pass, checking that the
+     * write was apparently successful. Not the most rigourous test, but it only takes
+     * two minutes to run.
      *
+     * Run from tape using initial instructions 40 0
+     *
+     
               @entry               * Define the entry address
           
      begin:            
-             26 index                                     
+             73 m2 : 30 mask       * Store address of this instruction to mark as the end of memory to test
+             23 m2 : 26 index      * Mask to keep bottom bits for comparison later
+                                            
      loop:   22 index / 30 message * Get next character 
              42 end   : 40 write   * Check for zero at end of string or write char 
      write:  20 char  / 74 4096    * Write character to teletype
              40 loop               * Loop back to next character  
      end:     
-               
+         
+     memtest:      
              * Start memory testing
              30 m1 : 20 mempos     * mempos := m1                      
-     blockloop:                      
-             * Start new block
-             26 wordsdoneinblock    * wordsdoneinblock := 0
-             30 testwordtouse : 05 testword1 * acc := testwordtouse - testword1
-             42 useword2 : 40 useword1 * if acc := 0 goto useword2, goto useword1
-     useword2:
-             30 testword2 : 20 testwordtouse
-             40 wordloop
-     useword1:
-             30 testword1 : 20 testwordtouse
-             40 wordloop        
      wordloop:
-             30 testwordtouse          * acc := testwordtouse
-             22 mempos / 20 0      * mempos++ / *mempos := acc
+             ** Test using first test byte
+             30 testword           * acc := testword1
+             22 mempos / 20 0      * ++mempos / *mempos := acc
              06 00                 * acc := 0
-             30 mempos / 30 0        * acc := mempos / acc := *mempos
-             05 testwordtouse : 42 correct
+             30 mempos / 30 0      * acc := mempos / acc := *mempos
+             05 testword : 42 correct
              40 printerror         * there was a problem. Print the error
      correct:
-             30 startlocation : 05 mempos  * acc := startlocation - mempos
-             42 finished : 22 wordsdoneinblock  * if acc = 0 goto finshed, wordsdoneinblock++
-             30 wordsdoneinblock : 05 blocksize * acc := wordsdoneinblock - blocksize
-             42 blockloop : 40 wordloop * if acc = 0 goto blockloop, goto wordloop
+             30 m2 : 05 mempos           * acc := m2 - mempos
+             42 finished : 40 wordloop   * if acc = 0 goto finshed, wordsdoneinblock++
          
      finished:
-             26 index              * index := 0
-     loop2:   22 index / 30 endmsg * Get next character 
-             42 end2   : 40 write2   * Check for zero at end of string or write char 
-     write2:  20 char  / 74 4096    * Write character to teletype
-             40 loop2               * Loop back to next character  
-     end2:    74 4125  : 74 4126    * Write CR and LF to finish line   
-             40 done               * exit program             
+             ** If this was the first pass, switch to the second test word and run again, testing the
+             ** opposite bits to last time
+             22 passes : 30 passes       * acc := ++passes
+             05 totalpasses : 42 exit    * if acc := 2 goto exit
+             30 testword2 : 20 testword  * testword := testword2
+             40 memtest                  * goto memtest
+     
+     exit:
+             ** Print success and then exit
+             26 index                    * index := 0
+     loop2:   22 index / 30 endmsg       * Get next character 
+             42 end2   : 40 write2       * Check for zero at end of string or write char 
+     write2:  20 char  / 74 4096         * Write character to teletype
+             40 loop2                    * Loop back to next character  
+     end2:    74 4125  : 74 4126         * Write CR and LF to finish line   
+             40 done                     * exit program             
      
      printerror:
-             26 index              * index := 0
-     loop3:   22 index / 30 errmsg * Get next character 
-             42 end3   : 40 write3   * Check for zero at end of string or write char 
-     write3:  20 char  / 74 4096    * Write character to teletype
-             40 loop3               * Loop back to next character  
-     end3:    74 4125  : 74 4126    * Write CR and LF to finish line   
-             40 done               * exit program           
+             26 index                    * index := 0
+     loop3:   22 index / 30 errmsg       * Get next character 
+             42 end3   : 40 write3       * Check for zero at end of string or write char 
+     write3:  20 char  / 74 4096         * Write character to teletype
+             40 loop3                    * Loop back to next character  
+     end3:    74 4125  : 74 4126         * Write CR and LF to finish line   
+             40 done                     * exit program           
              
      entry:  
-             40 begin              * goto begin
+             26 passes: 40 begin         * passes := 0, goto begin
               
-     done:   72 8191               * Exit when finished     
+     done:   40 done                     * Spin loop
 
     *************************************************************
+    
      char:   0                     * Character workspace
      index:  0                     * Index into string
-     message: 'Testing 0 to 4095... '
+     message: 'Testing '
              0                     * Zero marks end of string 
      endmsg: 'Success'
              0                     * Zero marks end of string 
      errmsg: 'Error'
              0                     * Zero marks end of string 
-     m1:    2                      * The first word to be tested - 1
+     m1:     3                     * The first word to be tested - 1
+     m2:     0                     * The last word to be tested, determined at runtime
      zero:   0
      mempos: 0                     * Current position in memory
-     testword1: 65535              * First word to write to memory
-     testword2:  -1                * Second word to write to memory
-     testwordtouse: 0
-     blocksize: 512                * Size in words to test before printing output
-     wordsdoneinblock: 0           * Number of words left in the current block
-     startlocation: 4095              * The first word of this program, stored at the beginning
-     
+     testword: 1099511627775       * First word to write to memory. 2^40-1
+     testword2: 0                  * Word to write on second pass
+     startlocation: 4095           * The first word of this program, stored at the beginning
+     passes:  0                    * Number of passes completed
+     totalpasses: 2                * Total number of passes we want
+     mask:   8191                  * Mask for address
